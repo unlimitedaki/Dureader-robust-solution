b11d32ec5d1edc4a796ea56539c0895678c58da4
Aki <unlimitedaki@gmail.com>
2020-04-06 13:40:12 +0800

    add pretraining on other dataset

--- run_dureader_robust_roberta.sh
+++ run_dureader_robust_roberta.sh
@@ -1,21 +1,23 @@
 python run_dureader_robust_roberta_large.py \
     --data_dir "dataset" \
-    --train_file "train.json" \
+    --train_file "DRCD_train.json" \
     --dev_file "dev.json" \
     --test_file "test1.json" \
     --output_dir "model" \
-    --model_name_or_path 'chinese-roberta' \
-    --save_model_name 'roberta-large-chinese-dureader-robust' \
+    --save_model_name 'roberta-large-multi-finetune-CMRC&dureader' \
+    --origin_model 'roberta-large-multi-finetune-CMRC&dureader/current_model_dureader' \
+    --target_model current_model \
     --do_train \
-    --do_finetune \
+    --do_eval \
     --max_seq_length 512 \
     --max_answer_length 20 \
     --doc_stride 128 \
-    --gradient_accumulation_steps 3 \
+    --gradient_accumulation_steps 4 \
     --num_train_epochs 5\
     --adam_epsilon 1e-8 \
     --learning_rate 3e-5 \
     --warmup_steps 0 \
-    --train_batch_size 12\
+    --train_batch_size 8\
     --eval_batch_size 12\
     --n_best_size 20\
+    --threads 8 \
